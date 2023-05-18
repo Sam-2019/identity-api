9863bcd77501c87a0dfaddf1577fed9101c70ebc
Samuel Martey <sam@encodevlabs.com>
2023-05-18 06:13:32 +0000

    config update

--- utils/config.js
+++ utils/config.js
@@ -5,14 +5,22 @@ const PROD_DB = process.env.PROD_DB;
 const AUTH_KEY = process.env.AUTH_KEY;
 const NODE_ENV = process.env.NODE_ENV;
 const HTTPPORT = process.env.HTTPPORT;
-const HTTPSPORT = process.env.HTTPSPORT
+const HTTPSPORT = process.env.HTTPSPORT;
 const TRUECALLER = process.env.TRUECALLER;
 
+const NOTIFY = (instance, port) => {
+ console.log(
+  NODE_ENV === "production"
+   ? `server live`
+   : `server live on ${instance}://localhost:${port}`
+ );
+};
 
 module.exports = {
  PORT,
  NIMBLE,
  DEV_DB,
+ NOTIFY,
  PROD_DB,
  AUTH_KEY,
  NODE_ENV,
