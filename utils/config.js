48e864c678f8cac29966f2faa63f49eaff79c9d3
Samuel Martey <sam@encodevlabs.com>
2023-05-18 05:58:21 +0000

    config update

--- utils/config.js
+++ utils/config.js
@@ -1,17 +1,22 @@
 const PORT = process.env.PORT;
-const DEV_DB = process.env.DEV_DB;
 const NIMBLE = process.env.NIMBLE;
+const DEV_DB = process.env.DEV_DB;
+const PROD_DB = process.env.PROD_DB;
 const AUTH_KEY = process.env.AUTH_KEY;
 const NODE_ENV = process.env.NODE_ENV;
-const PROD_DB = process.env.PROD_DB;
+const HTTPPORT = process.env.HTTPPORT;
+const HTTPSPORT = process.env.HTTPSPORT
 const TRUECALLER = process.env.TRUECALLER;
+
 
 module.exports = {
  PORT,
- DEV_DB,
  NIMBLE,
+ DEV_DB,
  PROD_DB,
- NODE_ENV,
  AUTH_KEY,
+ NODE_ENV,
+ HTTPPORT,
+ HTTPSPORT,
  TRUECALLER,
 };
