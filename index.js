75a87753a5bd58612c8ed4e7219424637c58096c
Samuel Martey <sam@encodevlabs.com>
2023-05-19 10:37:17 +0000

    cert files

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 require("dotenv").config();
+var https = require("https");
+var http = require("http");
+var fs = require("fs");
 const app = require("./app");
-const { PORT, NODE_ENV } = require("./utils/config");
+const { HTTPPORT, HTTPSPORT, NOTIFY } = require("./utils/config"); 
 
-const port = PORT || 4000;
+const httpPort = HTTPPORT || 80;
+const httpsPort = HTTPSPORT || 443;
 
-app.listen(port, () => {
- console.log(
-  NODE_ENV === "production"
-   ? `server live`
-   : `server live on http://localhost:${port}`
- );
-});
+const options = {
+  key: fs.readFileSync('test/fixtures/keys/client-key.pem'),
+  cert: fs.readFileSync('test/fixtures/keys/client-cert.pem'),
+};
+
+// // Create an HTTP service.
+http.createServer(app).listen(httpPort , () => { NOTIFY('http', httpPort) })
+
+// Create an HTTPS service identical to the HTTP service.
+https.createServer(options, app).listen(httpsPort, () => { NOTIFY('https', httpsPort) });
