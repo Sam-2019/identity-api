5987bea03f77c7247778b1858f375d46d0fe5415
Samuel Martey <sam@encodevlabs.com>
2023-05-20 17:01:37 +0000

    deploy update

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,13 +3,14 @@ require 'mina/git'
 require 'mina/rvm'
 require_relative 'environment'
 
-APPS_ROOT = "~/sites"
-
 # Basic settings:
+  set :application_name, "#{ENV['app']}"
   set :domain, "#{ENV['domain']}"
+  set :deploy_to, "#{ENV['home_dir']}/#{ENV['app']}"
   set :rvm_use_path, '/usr/local/rvm/scripts/rvm'
 
 # Optional settings:
+  set :term_mode, :pretty
   set :user, "#{ENV['user']}"         # Username in the server to SSH to.
 
 # Shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
@@ -22,14 +23,25 @@ APPS_ROOT = "~/sites"
 # `mina deploy` or `mina rake`.
 desc "sets ruby version"
 task :remote_environment do
-  invoke :'rvm:use', "#{ENV['ruby-version']}"
+  invoke :'rvm:use', "#{ENV['ruby_version']}"
 end
 
 # Put any custom commands you need to run at setup
 # All paths in `shared_dirs` and `shared_paths` will be created on their own.
 desc "set deployment directory"
 task :setup do
-  command %(mkdir -p #{APPS_ROOT})
+  command %(mkdir -p #{ENV['home_dir']})
+end
+
+desc "Deploys the current app version to the server."
+task :deploy do
+  deploy do
+    comment "Deploying #{fetch(:application_name)} to #{fetch(:domain)}:#{fetch(:deploy_to)}}"
+
+    on :launch do
+      invoke :remote_environment
+    end
+  end
 end
 
 desc "spits comments"
