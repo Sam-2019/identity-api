4f3ee5d01dff6033ede8dad7387ea1b74d30dd40
Samuel Martey <sam@encodevlabs.com>
2023-05-26 19:21:55 +0000

    fix cron error

--- services/cron.js
+++ services/cron.js
@@ -1,9 +1,15 @@
 import cron from "node-cron";
 import { sendTweet } from "./twitter.js";
 
-var task = cron.schedule("30 9 * * *", sendTweet(), {
- scheduled: true,
- timezone: "Africa/Accra",
-});
+var task = cron.schedule(
+ "30 9 * * *",
+ function () {
+  sendTweet();
+ },
+ {
+  scheduled: true,
+  timezone: "Africa/Accra",
+ }
+);
 
 export { task };
