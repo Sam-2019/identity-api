646f304e1a50039ede86454481dbc1d95a34f1ce
Samuel Martey <sam@encodevlabs.com>
2023-06-02 18:37:12 +0000

    deploy cleanup

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,23 +20,12 @@ set :service_unit_path, '/etc/systemd/system'
 # Optional settings:
 set :term_mode, :pretty
 set :user, ENV['user'].to_s
-# Username in the server to SSH to.
 
-# Shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
-# Some plugins already add folders to shared_dirs like `mina/rails` add `public/assets`, `vendor/bundle` and many more
-# run `mina -d` to see all folders and files already included in `shared_dirs` and `shared_files`
-# set :shared_dirs, fetch(:shared_dirs, []).push('public/assets')
-# set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
-
-# This task is the environment that is loaded for all remote run commands, such as
-# `mina deploy` or `mina rake`.
 desc 'sets ruby version'
 task :remote_environment do
   invoke :'rvm:use', ENV['ruby_version'].to_s
 end
 
-# Put any custom commands you need to run at setup
-# All paths in `shared_dirs` and `shared_paths` will be created on their own.
 desc 'set deployment directory'
 task :setup do
   command %(mkdir -p #{ENV['home_dir']})
@@ -98,17 +87,6 @@ task :shutdown do
   end
 end
 
-desc 'redis console'
-task redis: :remote_environment do
-  command 'redis-cli'
-end
-
-desc 'switch sms processor'
-task switch_sms_processor: :remote_environment do; end
-
-desc 'db backup'
-task backup: :remote_environment do; end
-
 desc 'show logs'
 task logs: :remote_environment do
   command %(pm2 logs --line 20 --time  0)
@@ -124,13 +102,18 @@ task pm2_dashboard: :remote_environment do
   command %(pm2 monit)
 end
 
-task memory_usage: :remote_environment do
-  in_path(fetch(:current_path)) do
-    command %(ps aux | head -1; ps aux | sort -rnk 4 | head -10)
-  end
+desc 'redis console'
+task redis: :remote_environment do
+  command 'redis-cli'
 end
 
 desc 'ngnix config'
 task edit_config: :remote_environment do
   command %(sudo nano /etc/nginx/sites-available/default)
 end
+
+desc 'switch sms processor'
+task switch_sms_processor: :remote_environment do; end
+
+desc 'db backup'
+task backup: :remote_environment do; end
