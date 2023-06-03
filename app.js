9c6ac31854b64703fbad48d4e2cfdb5edff5d2a7
Samuel Martey <sam@encodevlabs.com>
2023-06-03 14:00:15 +0000

    added redis

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ import express, { json } from "express";
 import router from "./routes/index.js";
 import { connectDB } from "./db/index.js";
 import { task } from "./services/cron.js";
+import { redisClient } from "./services/redis.js";
 
 const app = express();
 connectDB();
+await redisClient.connect();
+
 task.start();
 
 app.use(cors({ origin: true, credentials: true }));
