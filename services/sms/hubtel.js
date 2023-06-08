58dfbc56d9e2e23f61036f91571eed47dc2e63f0
Samuel Martey <sam@encodevlabs.com>
2023-06-08 18:02:51 +0000

    added hubtel

---
+++ services/sms/hubtel.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import {
+ HUBTEL_CLIENT_ID,
+ HUBTEL_SENDER_ID,
+ HUBTEL_CLIENT_SECRET,
+} from "../../utils/config.js";
+
+async function smsHubtel({ from = HUBTEL_SENDER_ID, to, message }) {
+ try {
+  const { data } = await axios.get(
+   "https://devp-sms03726-api.hubtel.com/v1/messages/send",
+   {
+    params: {
+     From: from,
+     To: to,
+     Content: message,
+     ClientId: HUBTEL_CLIENT_ID,
+     ClientSecret: HUBTEL_CLIENT_SECRET,
+    },
+   }
+  );
+  console.log(data);
+ } catch (error) {
+  console.error(error.response.data);
+ }
+}
+
+export { smsHubtel };
+
+// smsHubtel({ to: "to", message: "message" });
+
