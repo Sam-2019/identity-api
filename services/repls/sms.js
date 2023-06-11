592fb5f0a72d7635b320fff70fa70a0f230edd9c
Samuel Martey <sam@encodevlabs.com>
2023-06-11 20:29:43 +0000

    updated sms

--- services/repls/sms.js
+++ services/repls/sms.js
@@ -1,17 +1,42 @@
 import RedisService from "./redis.js";
+import { smsHellio } from "../sms/hellio.js";
+import { smsPusher } from "../sms/smspusher.js";
+import { smsHubtel } from "../sms/hubtel.js";
 
 class SmsProcessor {
  constructor() {
   this.redis = new RedisService();
  }
 
-// list sms providers
-// switch input must be in list
-// return default provider if input is foreign  
-
  //  default_sms_processor
  static default() {
   return "HUBTEL";
+ }
+
+ // list sms providers
+ // switch input must be in list
+ // return default provider if input is foreign
+
+ //  await sms_processor.send_sms("0240586043", "hi")
+ async send_sms(to, message) {
+  if (to === "" || message === "") return;
+  
+  const processors = ["HELLIO", "SMSPUSHER", "HUBTEL"];
+  const processor = await this.redis.read("sms_processor");
+  const validateProcessor = processors.includes(processor);
+  if (validateProcessor === false) return;
+
+  const current_processor = processor ?? "HUBTEL";
+  switch (current_processor) {
+   case "HELLIO":
+    smsHellio({ to, message });
+    break;
+   case "SMSPUSHER":
+    smsPusher({ to, message });
+    break;
+   default:
+    smsHubtel({ to, message });
+  }
  }
 
  //  await sms_processor.current_processor()
