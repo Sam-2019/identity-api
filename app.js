adb0df357f060d3c4951e616d5ce16de49bd5d0c
Samuel Martey <sam@encodevlabs.com>
2023-06-14 09:53:36 +0000

    added helmet

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import cors from "cors";
 import express, { json } from "express";
 import router from "./routes/index.js";
+import helmet from "helmet";
 import { connectDB } from "./db/index.js";
 import { task } from "./services/cron.js";
 import { redisClient } from "./services/redis.js";
 
 const app = express();
+app.use(helmet());
 app.use(cors({ origin: true, credentials: true }));
 app.use(json());
 
