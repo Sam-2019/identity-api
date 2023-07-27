ef7b9ac88ac6887bdcc0c0df2822e62904ab23e6
Samuel Martey <sam@encodevlabs.com>
2023-07-27 16:59:47 +0000

    added retweet & tweet

--- services/twitter.js
+++ services/twitter.js
@@ -1,5 +1,9 @@
 import { TwitterApi } from "twitter-api-v2";
-import { addTweet } from "../db/repository/tweet.js";
+import {
+ addTweet,
+ getNonRetweet,
+ updateTweet,
+} from "../db/repository/tweet.js";
 
 const client = new TwitterApi({
  appKey: process.env.TWITTER_API_KEY,
@@ -10,14 +14,39 @@ const client = new TwitterApi({
 
 const sendTweet = async () => {
  const tweet = "grateful";
+
  try {
   const response = await client.v2.tweet(tweet);
-  await addTweet(tweet, "success", response);
+  await addTweet(tweet, "success", false, response);
   console.log("post successful");
  } catch (e) {
-  await addTweet(tweet, "failed", e.data);
+  await addTweet(tweet, "failed", false, e.data);
   console.log("error");
  }
 };
 
-export { sendTweet };
+const retweet = async (tweetID) => {
+ // loggedUserId: string: Logged user (you) ID
+ const loggedUserId = "12";
+ try {
+  await client.v2.retweet(loggedUserId, tweetID);
+  await updateTweet(true, data.id);
+  console.log("retweet successful");
+ } catch (e) {
+  await updateTweet(false, data.id);
+  console.log("retweet failed");
+ }
+};
+
+const tweet = async () => {
+ const data = await getNonRetweet();
+
+ if (data) {
+  const tweetID = data.payload.edit_history_tweet_ids.id;
+  return retweet(tweetID);
+ }
+
+ sendTweet();
+};
+
+export { tweet };
