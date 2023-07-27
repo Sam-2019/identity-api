b7b10caba935ee8d3d5b4876372b92a87536e963
Samuel Martey <sam@encodevlabs.com>
2023-07-27 16:57:49 +0000

    added more repository

--- db/repository/tweet.js
+++ db/repository/tweet.js
@@ -4,16 +4,28 @@ const getAll = async () => {
  return await Model.find({});
 };
 
+const getNonRetweet = async () => {
+ return await Model.findOne({
+  retweeted: false,
+ });
+};
+
 const getTweet = async (tweet) => {
  return await Model.findOne({ tweet });
 };
 
-const addTweet = async (tweet, status, payload) => {
+const addTweet = async (tweet, status, retweeted, payload) => {
  return await Model.create({
   tweet,
   status,
+  retweeted,
   payload,
  });
 };
 
-export { getAll, getTweet, addTweet };
+const updateTweet = async (retweeted, id) => {
+ const query = { id: id };
+ return await Model.findOneAndUpdate(query, { $set: { retweeted: retweeted } });
+};
+
+export { getAll, getNonRetweet, getTweet, addTweet, updateTweet };
