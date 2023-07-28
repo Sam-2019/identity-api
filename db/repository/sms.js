046599259bb1911b7ace1972bf356611a243b680
Samuel Martey <sam@encodevlabs.com>
2023-07-28 14:45:13 +0000

    added sms schema, model & repository

---
+++ db/repository/sms.js
@@ -0,0 +1,21 @@
+import Model from "../model/sms";
+
+const getAll = async () => {
+ return await Model.find({});
+};
+
+const getOne = async (mobile_number) => {
+ return await Model.findOne({ mobile_number });
+};
+
+const addReceipt = async (mobile_number, message, from, provider, data) => {
+ return await Model.create({
+  mobile_number,
+  message,
+  from,
+  provider,
+  payload: data,
+ });
+};
+
+export { getAll, getOne, addReceipt };
