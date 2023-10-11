016b64db66fdc4cf23eee1d3f68cf05702f37679
Sam <samtmartey@gmail.com>
2023-10-11 20:56:32 +0100

    corrected service call

--- services/repls/redis.js
+++ services/repls/redis.js
@@ -1,12 +1,12 @@
 import { redisClient } from "../redis.js";
 
 class RedisService {
- // await redis.write("key", "xxxxx")
+ // await redis_service.write("key", "xxxxx")
  async write(key, value) {
   return await redisClient.set(key, value);
  }
 
- // await redis.read("key")
+ // await redis_service.read("key")
  async read(key) {
   return await redisClient.get(key);
  }
